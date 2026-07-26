28f3775e0ebed504c0742edf132c9fb1f78249d9
DenomSly <denomsly2@gmail.com>
2026-07-26 07:55:24 +0700

    cpufreq: schedutil: tune rate limit defaults
    Adjust schedutil up and down rate limits to improve CPU
    frequency response and maintain balanced power efficiency.
    
    Reduce frequency ramp-up delay for faster UI response while
    keeping frequency ramp-down controlled to avoid unnecessary
    oscillation.
    
    Signed-off-by: DenomSly <denomsly2@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -875,8 +875,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us   = 250;
-    tunables->down_rate_limit_us = 1500;
+	tunables->up_rate_limit_us   = 500;
+    tunables->down_rate_limit_us = 2000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
